d52721f21151b8da1bd6acee51e3d760cdf071e2
DKSecurity <55659237+DKSecurity99@users.noreply.github.com>
2020-09-19 21:41:51 -0300

    Delete

--- public/css/index.js
+++
@@ -1,6 +0,0 @@
-import initializeBanknoteControlProcess from './controller/NotesController.js';
-import initializeViewNotes from './views/NotesView.js';
-
-initializeBanknoteControlProcess();
-initializeViewNotes();
-
